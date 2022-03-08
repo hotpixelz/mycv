593f6ffb42d6d854b689cc37639db1681db60552
Venelin Vasilev <venelin.vasilev@analogfolk.com>
2022-03-08 10:37:18 +0200

    Added prod config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,6 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 const cookieSession = require('cookie-session');
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
